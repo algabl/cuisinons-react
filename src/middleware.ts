144a681f832882cf16c59d12020729e9d174d573
Alex Black <ablack00@byu.edu>
2025-05-21 16:31:01 -0600

    hmm

--- src/middleware.ts
+++ src/middleware.ts
@@ -7,6 +7,10 @@ export function middleware(request: NextRequest) {
     request.cookies.get("authjs.session-token");
   const { pathname } = new URL(request.url);
 
+  if (pathname.startsWith("/_next/") || pathname.startsWith("/public/")) {
+    return NextResponse.next();
+  }
+
   if (pathname === "/login" && authHeader) {
     return NextResponse.redirect(new URL("/app", request.url));
   }
@@ -19,5 +23,11 @@ export function middleware(request: NextRequest) {
 }
 
 export const config = {
-  matcher: ["/((?!api|_next|public|$).*)"], // Protect all routes except API, _next, public assets, and "/"
+  matcher: [
+    // Exclude all files in the /public folder
+    // Exclude all files in the /api folder
+    // Exclude _next/
+    // Exclude favicon.ico
+    "/((?!api|_next|static|public|favicon.ico).*)",
+  ],
 };
