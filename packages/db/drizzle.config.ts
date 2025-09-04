8f2697514e377163e96b0c2fca650da1a7584faf
Alex Black <ablack00@byu.edu>
2025-09-04 14:13:47 -0600

    some more fixies for deployment

--- packages/db/drizzle.config.ts
+++ packages/db/drizzle.config.ts
@@ -1,12 +1,13 @@
 import { type Config } from "drizzle-kit";
+import path from "path";
 
 if (!process.env.NEON_DATABASE_URL) {
   throw new Error("Missing NEON_DATABASE_URL");
 }
 
 export default {
-  schema: "./src/schema.ts",
-  dialect: "postgresql",
+    schema: path.resolve(__dirname, "./src/schema.ts"),
+    dialect: "postgresql",
   dbCredentials: {
     url: process.env.NEON_DATABASE_URL,
   },
