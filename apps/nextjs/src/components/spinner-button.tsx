c345a4065d0788c1e4afcbc69fd97bf29daab259
Alex Black <ablack00@byu.edu>
2025-09-16 21:25:52 -0600

    updated spinner button

--- apps/nextjs/src/components/spinner-button.tsx
+++ apps/nextjs/src/components/spinner-button.tsx
@@ -1,13 +1,16 @@
 import React from "react";
-import { Button, type ButtonProps } from "./ui/button";
+
+import type { ButtonProps } from "./ui/button";
+import { Button } from "./ui/button";
 import { Spinner } from "./ui/spinner";
+
 export const SpinnerButton = ({
   loading = false,
   children,
   ...props
 }: { loading?: boolean } & ButtonProps) => (
   <Button disabled={loading} {...props}>
+    {loading && <Spinner className="text-secondary p-1" />}
     {loading ? "Loading..." : (children ?? "Submit")}
-    {loading && <Spinner />}
   </Button>
 );
