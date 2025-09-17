676a70a2aa67f970e390c5f6d798386cd16be360
Alex Black <ablack00@byu.edu>
2025-09-17 17:33:49 -0600

    fixed posthog

--- apps/nextjs/src/env.js
+++ apps/nextjs/src/env.js
@@ -27,6 +27,8 @@ export const env = createEnv({
    */
   client: {
     // NEXT_PUBLIC_CLIENTVAR: z.string(),
+    NEXT_PUBLIC_POSTHOG_KEY: z.string(),
+    NEXT_PUBLIC_POSTHOG_HOST: z.string().url().optional(),
   },
 
   /**
@@ -40,6 +42,8 @@ export const env = createEnv({
     NEON_DATABASE_URL: process.env.NEON_DATABASE_URL,
     NODE_ENV: process.env.NODE_ENV,
     OPENROUTER_API_KEY: process.env.OPENROUTER_API_KEY,
+    NEXT_PUBLIC_POSTHOG_KEY: process.env.NEXT_PUBLIC_POSTHOG_KEY,
+    NEXT_PUBLIC_POSTHOG_HOST: process.env.NEXT_PUBLIC_POSTHOG_HOST,
   },
   /**
    * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially
