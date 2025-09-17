d0a806ef07e22aaebc683416215693c091748057
Alex Black <ablack00@byu.edu>
2025-09-17 15:53:11 -0600

    first try adding posthog

--- apps/nextjs/next.config.js
+++ apps/nextjs/next.config.js
@@ -21,6 +21,22 @@ const config = {
   experimental: {
     authInterrupts: true,
   },
+
+  async rewrites() {
+    return [
+      {
+        source: "/ingest/static/:path*",
+        destination: "https://us-assets.i.posthog.com/static/:path*",
+      },
+      {
+        source: "/ingest/:path*",
+        destination: "https://us.i.posthog.com/:path*",
+      },
+    ];
+  },
+
+  // This is required to support PostHog trailing slash API requests
+  skipTrailingSlashRedirect: true,
 };
 
 export default config;
