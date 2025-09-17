d1e1bfd8e590fac49f8c729e7e21141fe3b682bc
Alex Black <ablack00@byu.edu>
2025-09-16 18:10:40 -0600

    enabled authinterrupts

--- apps/nextjs/next.config.js
+++ apps/nextjs/next.config.js
@@ -18,6 +18,9 @@ const config = {
   /** We already do linting and typechecking as separate tasks in CI */
   eslint: { ignoreDuringBuilds: true },
   typescript: { ignoreBuildErrors: true },
+  experimental: {
+    authInterrupts: true,
+  },
 };
 
 export default config;
