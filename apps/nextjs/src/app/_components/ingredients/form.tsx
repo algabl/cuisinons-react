2aa7102627316a95a9d8467fcf267be0a789ed70
Alex Black <ablack00@byu.edu>
2025-09-16 21:23:52 -0600

    split out ingredient form

---
+++ apps/nextjs/src/app/_components/ingredients/form.tsx
@@ -0,0 +1,89 @@
+"use client";
+
+import type { z } from "zod/v4";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+
+import type { IngredientFormData } from "@cuisinons/api/types";
+
+import { Button } from "~/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "~/components/ui/form";
+import { Input } from "~/components/ui/input";
+import { ingredientFormSchema } from "~/lib/validations";
+import { api } from "~/trpc/react";
+
+export type IngredientPrefill = Partial<z.infer<typeof ingredientFormSchema>>;
+
+export default function IngredientForm({
+  onSubmit,
+  prefill,
+  isLoading = false,
+}: {
+  onSubmit: (ingredient: IngredientFormData) => void;
+  prefill?: IngredientPrefill;
+  isLoading?: boolean;
+}) {
+  const form = useForm({
+    resolver: zodResolver(ingredientFormSchema),
+    defaultValues: {
+      name: prefill?.name ?? "",
+      description: prefill?.description ?? "",
+    },
+  });
+
+  return (
+    <Form {...form}>
+      <div className="space-y-4">
+        {/* Name */}
+        <FormField
+          control={form.control}
+          name="name"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Name</FormLabel>
+              <FormControl>
+                <Input placeholder="Ingredient name" {...field} />
+              </FormControl>
+              <FormDescription>
+                This is the ingredient&apos;s name.
+              </FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        {/* Description */}
+        <FormField
+          control={form.control}
+          name="description"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Description</FormLabel>
+              <FormControl>
+                <Input placeholder="An ingredient for..." {...field} />
+              </FormControl>
+              <FormDescription>
+                This is the ingredient&apos;s description.
+              </FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button
+          type="button"
+          onClick={form.handleSubmit(onSubmit)}
+          disabled={isLoading}
+        >
+          {isLoading ? "Saving..." : "Submit"}
+        </Button>
+      </div>
+    </Form>
+  );
+}
